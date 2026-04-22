506e4b9b9d0b2cbb9bae326e4efb9f1c3a7be95f
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-04-22 09:08:03 -0300

    feat: create, delete e list de classes

--- src/controllers/ClassControllers.ts
+++ src/controllers/ClassControllers.ts
@@ -5,7 +5,7 @@ import { Class } from "../entities/Class";
 export class ClassControllers{
     public static async get(req: Request, res: Response){
         try{
-
+            const repo = Connection.getRepository(Class)
         }catch(error: any){
             return res.status(500).json({
                 status: false,
@@ -16,7 +16,14 @@ export class ClassControllers{
 
     public static async listClass(req: Request, res: Response){
         try{
+            const repo = Connection.getRepository(Class)
 
+            const classes = await repo.find()
+
+            return res.status(200).json({
+                status: true,
+                data: classes 
+            })
         }catch(error: any){
             return res.status(500).json({
                 status: false,
@@ -27,7 +34,7 @@ export class ClassControllers{
 
     public static async createClass(req: Request, res: Response){
         try{
-
+              const repo = Connection.getRepository(Class)
         }catch(error: any){
             return res.status(500).json({
                 status: false,
@@ -38,7 +45,7 @@ export class ClassControllers{
 
     public static async updateClass(req: Request, res: Response){
             try{
-
+                  const repo = Connection.getRepository(Class)
         }catch(error: any){
             return res.status(500).json({
                 status: false,
@@ -49,7 +56,7 @@ export class ClassControllers{
 
     public static async deleteClass(req: Request, res: Response){
             try{
-
+                  const repo = Connection.getRepository(Class)
         }catch(error: any){
             return res.status(500).json({
                 status: false,
