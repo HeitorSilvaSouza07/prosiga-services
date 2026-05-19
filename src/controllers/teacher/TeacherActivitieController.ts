1c9f3e5659d801940c5235796cd5a24451de6d01
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-05-19 14:57:10 -0300

    feat: atualização do repositorioe novos endpoints

---
+++ src/controllers/teacher/TeacherActivitieController.ts
@@ -0,0 +1,29 @@
+ import { Request, Response } from "express";
+ import { Connection } from "../../database/dataBase";
+ import { Activitie } from "../../entities/Activitie";
+ import { User } from "../../entities/User";
+ import { Class } from "../../entities/Class";
+
+ export class TeacherActivitieController{
+    static async createActivitie(req: Request, res: Response){
+
+        
+        
+    }
+
+    static async updateActivitie(req: Request, res: Response){
+
+    }
+
+    static async desactiveActivitie(req: Request, res: Response){
+
+    }
+
+    static async listActivitie(req: Request, res: Response){
+        
+    }
+
+    static async getActivitie(req: Request, res: Response){
+
+    }
+ }
